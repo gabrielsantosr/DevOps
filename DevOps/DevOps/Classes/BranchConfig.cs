a176ca55e4e9a825f6750189a34f1936408312fa
Gabriel Santos <gabriel.santos@globalix365.tech>
2025-11-02 04:43:59 -0300

    New branch transition configuration proposal

---
+++ DevOps/DevOps/Classes/BranchConfig.cs
@@ -0,0 +1,33 @@
+﻿namespace DevOps.Classes
+{
+    public class BranchConfig
+    {
+        public string DefaultTarget { get; set; }
+        public string AllowedSourcesRegex { get; set; }
+    }
+}
+/*
+ * Proposed new config structure
+{
+	"MyRepo1Name": {
+		"master": {
+			"defaultTarget": "",
+			"allowedSourcesRegex": "regex"
+		},
+		"dev": {
+			"defaultTarget": "master",
+			"allowedSourcesRegex": "regex"
+		}
+	},
+	"MyRepo2Name": {
+		"master": {
+			"defaultTarget": "",
+			"allowedSourcesRegex": "regex"
+		},
+		"dev": {
+			"defaultTarget": "master",
+			"allowedSourcesRegex": "regex"
+		}
+	}
+}
+ */
